033dad0a7122538352ae7aca11c5a4d1ef64f440
Bao Tran <tranbao11234@gmail.com>
2025-01-10 16:03:51 +0700

    [BE] Config sequelize and connect to DB

--- Clinic_BE/src/index.ts
+++ Clinic_BE/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import configViewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
+import connectDB from "./config/connectDB";
 require("dotenv").config();
 
 const app = express();
@@ -12,6 +13,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 configViewEngine(app);
 initWebRoutes(app);
+
+// Connect to DB
+connectDB();
 
 const port = process.env.PORT || 8082;
 
